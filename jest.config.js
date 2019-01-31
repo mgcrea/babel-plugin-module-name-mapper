f9cbe1cae1a52329f9abbc6e1ea0c91c6917b0fc
Olivier Louvignes <olivier@mgcrea.io>
2019-01-31 09:49:27 +0100

    feat(update): minor changes

--- jest.config.js
+++ jest.config.js
@@ -3,6 +3,8 @@
 module.exports = {
   roots: ['<rootDir>/src/', '<rootDir>/test/'],
   setupFiles: ['<rootDir>/test/setup.js'],
+  collectCoverageFrom: ['<rootDir>/src/**/*.js'],
+  // coveragePathIgnorePatterns: ["/node_modules/"],
   // setupFilesAfterEnv: ['<rootDir>/test/framework.ts'],
   testEnvironment: 'node'
 };
